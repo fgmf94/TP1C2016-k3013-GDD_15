6593461c6900bb50c85a476d2d5ea561b1f5395c
Federico Martínez <f_gmf94@hotmail.com>
2016-06-06 16:48:02 -0300

    continuo con los filtros de los clientes

--- VentaEnvioDesktop/WindowsFormsApplication1/ABM Usuario/FiltroCliente.cs
+++ VentaEnvioDesktop/WindowsFormsApplication1/ABM Usuario/FiltroCliente.cs
@@ -34,9 +34,32 @@ namespace WindowsFormsApplication1.ABM_Usuario
                 MessageBox.Show("Debe ingresar al menos algún filtro", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
             } else 
             {
-                wheres = "D_NOMBRES LIKE '%" + txtNombre.Text + "%'";
-                CompletadorDeTablas.hacerQuery("SELECT C_USUARIO_NOMBRE Username, D_NOMBRES Nombres, D_APELLIDOS Apellidos, N_DOCUMENTO DNI, C_CORREO Mail FROM GDD_15.USUARIOS U JOIN GDD_15.CLIENTES C ON (U.N_ID_USUARIO = C.N_ID_USUARIO) WHERE" + wheres, ref dataGridView1);
+                wheres = "";
+                armarWheres();
+                CompletadorDeTablas.hacerQuery("SELECT C_USUARIO_NOMBRE Username, D_NOMBRES Nombres, D_APELLIDOS Apellidos, N_DOCUMENTO DNI, C_CORREO Mail FROM GDD_15.USUARIOS U JOIN GDD_15.CLIENTES C ON (U.N_ID_USUARIO = C.N_ID_USUARIO) WHERE " + wheres, ref dataGridView1);
             }
+        }
+
+        public void armarWheres()
+        {
+            if(txtNombre.Text != "")
+            {
+                wheres = wheres + "D_NOMBRES LIKE '%" + txtNombre.Text + "%' AND ";
+            }
+            if (txtApellido.Text != "")
+            {
+                wheres = wheres + "D_APELLIDOS LIKE '%" + txtApellido.Text + "%' AND ";
+            }
+            if (txtEmail.Text != "")
+            {
+                wheres = wheres + "C_CORREO LIKE '%" + txtEmail.Text + "%' AND ";
+            }
+            if (txtDNI.Text != "")
+            {
+                wheres = wheres + "N_DOCUMENTO = '" + txtDNI.Text + "' AND ";
+            }
+
+            wheres = wheres.Substring(0, wheres.Length - 5);
         }
 
         private void buttonLimpiar_Click(object sender, EventArgs e)
