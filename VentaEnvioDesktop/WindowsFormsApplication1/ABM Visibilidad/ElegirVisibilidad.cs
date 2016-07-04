efc0432cd2d8292896296cfaa4fe99c2cd8491b9
Federico Martínez <f_gmf94@hotmail.com>
2016-07-04 04:35:52 -0300

    ahora no se pueden modificar las visibilidades utilizadas por publicaciones

--- VentaEnvioDesktop/WindowsFormsApplication1/ABM Visibilidad/ElegirVisibilidad.cs
+++ VentaEnvioDesktop/WindowsFormsApplication1/ABM Visibilidad/ElegirVisibilidad.cs
@@ -69,7 +69,7 @@ namespace WindowsFormsApplication1.ABM_Visibilidad
 
                 if (Convert.ToInt16(cant) != 0)
                 {
-                    MessageBox.Show("No se puede eliminar una visibilidad utilizada", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    MessageBox.Show("No se puede eliminar una visibilidad con publicaciones activas o pausadas", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     return;
                 }
 
@@ -87,6 +87,20 @@ namespace WindowsFormsApplication1.ABM_Visibilidad
             }
             else if (formato == "Modificar Visibilidad")
             {
+                string query4 = "SELECT C_VISIBILIDAD FROM GDD_15.VISIBILIDADES WHERE D_DESCRIP = '" + comboBoxVisi.Text + "'";
+                DataTable dt4 = (new ConexionSQL()).cargarTablaSQL(query4);
+                string visiID = dt4.Rows[0][0].ToString();
+
+                string query = "SELECT COUNT(N_ID_PUBLICACION) FROM GDD_15.PUBLICACIONES P JOIN GDD_15.ESTADOS E ON (E.N_ID_ESTADO = P.N_ID_ESTADO) WHERE C_VISIBILIDAD = '" + visiID + "' AND C_ESTADO IN ('Activa', 'Pausada')";
+                DataTable dt = (new ConexionSQL()).cargarTablaSQL(query);
+                string cant = dt.Rows[0][0].ToString();
+
+                if (Convert.ToInt16(cant) != 0)
+                {
+                    MessageBox.Show("No se puede modificar una visibilidad con publicaciones activas o pausadas", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
                 modificarVisi = new ABM_Visibilidad.ModificarVisibilidad(comboBoxVisi.Text, this);
                 modificarVisi.ShowDialog();
             }
